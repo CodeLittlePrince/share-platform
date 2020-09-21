c1dfdd746c9cfe3c7676097435930afb3f03dc73
nijunjie <nijunjie@corp.netease.com>
2020-09-21 16:08:47 +0800

    feat: 捕获异常

--- app/controller/home.js
+++ app/controller/home.js
@@ -12,25 +12,29 @@ class HomeController extends Controller {
   async share() {
     const { ctx } = this;
     const data = ctx.query;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-    const template = await ctx.renderView('globalpay/index', data);
+      const template = await ctx.renderView('globalpay/index', data);
 
-    await page.setContent(template);
+      await page.setContent(template);
 
-    const buffer = await page.screenshot({
-      type: 'png',
-      clip: {
-        x: 0,
-        y: 0,
-        width: 285,
-        height: 525,
-      },
-      // encoding: 'base64',
-    });
+      const buffer = await page.screenshot({
+        type: 'png',
+        clip: {
+          x: 0,
+          y: 0,
+          width: 285,
+          height: 525,
+        },
+      });
+      ctx.body = buffer;
+    } catch (error) {
+      ctx.logger.error(new Error(error));
 
-    ctx.body = buffer;
+      ctx.body = 'Home share function error';
+    }
   }
 }
 
